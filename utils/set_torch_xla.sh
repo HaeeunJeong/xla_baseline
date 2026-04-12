4af48efbc3fa34b6b99e5ef8cd321a28417eb138
HaeeunJeong <haeeunj10@gmail.com>
2026-04-12 16:44:36 +0900

    Fix install script

--- utils/set_torch_xla.sh
+++ utils/set_torch_xla.sh
@@ -2,7 +2,7 @@ git clone https://github.com/pytorch/pytorch.git
 cd pytorch
 git checkout 0fabc3b # 2.9.0
 
-git clone git clone https://github.com/pytorch/xla.git
+git clone https://github.com/pytorch/xla.git
 cd xla
 git checkout 5fab705 # 2.9.8-rc3
 
@@ -33,6 +33,12 @@ sudo fallocate -l 64G /swapfile2
 sudo chmod 600 /swapfile2
 sudo mkswap /swapfile2
 sudo swapon /swapfile2
+
+sudo apt install apt-transport-https curl gnupg -y
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt update && sudo apt install bazel-7.4.1
 
 cd ../pytorch/xla
 python setup.py bdist_wheel
